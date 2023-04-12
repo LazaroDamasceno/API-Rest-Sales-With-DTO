ba2bbea078bcc0027678c2fcd49c5fa90f1c9a1f
lazarodamasceno <lazarodamasceno2@gmail.com>
2023-04-11 23:25:53 -0300

    deleted the interface of the controller; reset the controller; fixed some errors relating to the exception handling

--- src/main/java/com/apirestsaleswithdto/controllers/SaveController.java
+++ src/main/java/com/apirestsaleswithdto/controllers/SaveController.java
@@ -1,17 +1,48 @@
 package com.apirestsaleswithdto.controllers;
 
+import com.apirestsaleswithdto.dtos.DtoMapper;
 import com.apirestsaleswithdto.dtos.SaleDTO;
 import com.apirestsaleswithdto.models.SaleModel;
+import com.apirestsaleswithdto.services.SaleService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-public interface SaveController {
+import static org.springframework.http.HttpStatus.OK;
 
-    ResponseEntity<List<SaleDTO>> getAll();
-    ResponseEntity<SaleDTO> getById(int id);
-    ResponseEntity<SaleModel> add(SaleModel model);
-    ResponseEntity<SaleModel> update(SaleModel model);
-    ResponseEntity<Void> deleteById(int id);
+@RestController
+@AllArgsConstructor
+@RequestMapping("/sales")
+public class SaveController {
+
+    final SaleService service;
+
+    @GetMapping
+    public ResponseEntity<List<SaleDTO>> getAll() {
+        return new ResponseEntity<>(DtoMapper.convertAll(service.getAll()), OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<SaleDTO> getById(@PathVariable(value = "id") int id) {
+        return new ResponseEntity<>(new SaleDTO(service.getById(id)), OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<SaleModel> add(@RequestBody SaleModel model) throws IllegalAccessException {
+        return new ResponseEntity<>(service.save(model), OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<SaleModel> update(@RequestBody SaleModel model) throws IllegalAccessException {
+        return new ResponseEntity<>(service.save(model), OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable(value = "id") int id) {
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
